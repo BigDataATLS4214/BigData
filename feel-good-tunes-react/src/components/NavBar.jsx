ba2e0eedd58964718dc89a359d8fde7e184c18b4
Kaitlyn Huynh <kahu1074@colorado.edu>
2023-02-28 22:58:24 -0700

    changed a bit of styling in pages

--- feel-good-tunes-react/src/components/NavBar.jsx
+++ feel-good-tunes-react/src/components/NavBar.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-import '../css/NavBar.css';
+import '../scss/NavBar.scss';
 
 
 
 export const NavBar = ({ setPage }) => {
     return (
       <div className="FullRow">
-          <h2>PlaylistGenie</h2>
+          <h2 className = 'home'>PlaylistGenie</h2>
           <div className="NavLinks">
             <button className="clickLinks" onClick={() => setPage('home')}>Home</button>
             <button className="clickLinks" onClick={() => setPage('playlists')}>Playlists</button>
           </div>
-          <button className="NavLinks" onClick={() => setPage('playlists')}>Login</button>
+          <button className="login-button" onClick={() => setPage('playlists')}>Login</button>
       </div>
     );
   };
