06e7e3ad5e4c715c812798c1f29e34e599f57efe
Kaitlyn Huynh <kahu1074@colorado.edu>
2023-03-01 12:20:46 -0700

    added more text and profile picture

--- feel-good-tunes-react/src/pages/Home.jsx
+++ feel-good-tunes-react/src/pages/Home.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import profile from '../img/profile-picture.png';
 
 export const Home = () => {
-    return <h1>Welcome to the Home page</h1>;
+  let resize = {
+    float: 'right',
+    width: '80%',
+    paddingTop: '6vw',
+  }
+    return(
+      <div className='container'>
+        <div className='half-page-left'>
+          <h2 className='songs-that-match'>SONGS THAT <br></br>MATCH YOUR MOOD</h2>
+        </div>
+        <div className='half-page-right'>
+          <img src = {profile} style = {resize} />
+        </div>
+      </div>
+    );
   };
