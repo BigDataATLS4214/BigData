87de324d131f5df807880d620e9d823357d208d0
Lola-Keychenko <lolakeychenko@engr2-26-204-dhcp.int.colorado.edu>
2023-04-05 16:25:22 -0600

    Connected to spotify api via spotipy

--- MSDB_playground/upload_info_to_db.py
+++ MSDB_playground/upload_info_to_db.py
@@ -1,18 +1,34 @@
 from decouple import config
 import pymongo
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+def spotify_login(cid, secret):
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
+    return spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+cid = "5d3513e3cd004a6ea6d5fd243247275c"
+secret = "540ce35cc89349948dbb8303d7647e29"
+
+sp = spotify_login(cid, secret)
 
 CLIENT = pymongo.MongoClient(config('ATLAS_URI'))
 DB = CLIENT["music_list"]
 
+result = sp.search("Radiohead", type='artist')
+
+
+
 def main():
-    file = open("MSDB_playground/track_info.txt", "r")
+    file = open("track_info.txt", "r")
     lines = file.readlines()
     for line in lines:
         temp = line.strip().split("<SEP>")
-        artist_id = insert_artist(temp[0])
+        #artist_id = insert_artist(temp[0])
         # TODO: use spotify api to find the genre(s), album(s), and emotion(s) the song belongs to
                 # then add them to db and get back resulting id to put into song_title 
-        insert_song(temp[1], artist_id)
+        #insert_song(temp[1], artist_id)
     
 
 # def insert_genre(genre):
