2e244401d92184b380533be04e8707fee3e5fa3f
MonteRyan61 <ryan.monteleone@colorado.edu>
2023-04-19 15:55:23 -0600

    streamlit writing to mongodb

--- ML/webCamApp.py
+++ ML/webCamApp.py
@@ -1,10 +1,21 @@
 import streamlit as st
 import cv2
+import pymongo
+from decouple import config
+from datetime import datetime
+import certifi
+
+CLIENT = pymongo.MongoClient(config('ATLAS_URI'), tlsCAFile=certifi.where())
+DB = CLIENT["music_list"]
+OUTPUTS = DB["outputs"]
 
 def main():
+    now = datetime.now()
     st.title("Webcam Test")
     run = st.checkbox('Run')
     video_feed = st.empty()
+    
+    OUTPUTS.insert_one({"name": "test", "createdAt": now})
     
     if run:
         cap = cv2.VideoCapture(0)
@@ -16,6 +27,8 @@ def main():
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             video_feed.image(frame)
         cap.release()
+        
+        
 
 if __name__ == '__main__':
     main()
