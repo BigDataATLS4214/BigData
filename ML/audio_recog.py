cd24f8a30867892305f98af90c594a584788476e
Ashwath Ramakrishnan <45356614+Ashwath99@users.noreply.github.com>
2023-04-24 11:45:00 -0600

    Add files via upload

--- ML/audio_recog.py
+++ ML/audio_recog.py
@@ -1,48 +1,54 @@
+import streamlit as st
 import speech_recognition as sr
-import pyttsx3
+from PIL import Image
+import os
 
 
-def speakText(cmd):
-    engine = pyttsx3.init()
-    engine.say(cmd)
-    engine.runAndWait()
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        if audio:
+            try:
+                txt = r.recognize_google(audio)
+                return txt
+            except:
+                st.write('Audio not recognized. Try again!')
+        else:
+            st.write('Audio not recognized. Try again!')
 
 
 def checkText(mytxt):
     mytxt = mytxt.split(' ')
     if 'happy' in mytxt:
-        return 1
+        return 1, 'happy'
     elif 'sad' in mytxt:
-        return 2
+        return 2, 'sad'
     elif 'angry' in mytxt:
-        return 3
+        return 3, 'angry'
     elif 'surprise' in mytxt:
-        return 4
+        return 4, 'surprise'
     elif 'neutral' in mytxt:
-        return 5
+        return 5, 'neutral'
     else:
-        return -1
+        return -1, ''
 
 def main():
-    recog = sr.Recognizer()
+    img = Image.open('microphone-solid.png')
+    st.image(img, width=75)
 
-    with sr.Microphone() as source:
-        recog.adjust_for_ambient_noise(source)
-        print('Say something')
-        audio = recog.listen(source)
-        print('Recognizing now...')
+    if st.button('Click to record audio'):
+        txt = get_audio()
+        print(txt)
+        if txt:
+            return_flag, genre = checkText(txt)
+            print(return_flag)
 
-        try:
-            mytxt = recog.recognize_google(audio)
-            mytxt = mytxt.lower()
-            print(f'Did you say: "{mytxt}"?')
-            speakText(mytxt)
-        except recog.UnknownValueError:
-            print('Unable to recognize speech')
-    
-    return_flag = checkText(mytxt)
-    print(return_flag)
-    
-    return return_flag
+            if return_flag != -1:
+                st.write("Recommending ", genre, " songs")
+            else:
+                st.write('Choose an available genre!')
+
+            return return_flag
 
 main()
