f262cb42ad2cf84ecd8cf2aa353910e94683fc23
MonteRyan61 <ryan.monteleone@colorado.edu>
2023-04-25 14:51:02 -0600

    added color to the nav bar page that the user is on for better UX

--- feel-good-tunes-react/src/components/NavBar.jsx
+++ feel-good-tunes-react/src/components/NavBar.jsx
@@ -17,8 +17,8 @@ export const NavBar = ({page, setPage, setCamState }) => {
       <div className="FullRow">
           <h2 className = 'home'>PlaylistGenie</h2>
           <div className="NavLinks">
-            <button className="clickLinks" style={{ textDecoration: page === 'home' ? 'underline' : 'none' }} onClick={handleHomeClick}>Home</button>
-            <button className="clickLinks" style={{ textDecoration: page === 'playlists' ? 'underline' : 'none' }} onClick={handlePlaylistClick}>Playlists</button>
+            <button className="clickLinks" style={{ textDecoration: page === 'home' ? 'underline' : 'none', color: page === 'home' ? '#534C6E' : 'black' }} onClick={handleHomeClick}>Home</button>
+            <button className="clickLinks" style={{ textDecoration: page === 'playlists' ? 'underline' : 'none', color: page === 'playlists' ? '#534C6E' : 'black'}} onClick={handlePlaylistClick}>Playlists</button>
           </div>
           <div className="navPlaceHolder"></div>
       </div>
